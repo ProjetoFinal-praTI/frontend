9083e0efc60a5cde812044ce361ffae60dbabf4e
lauranarciso <lauranarcisop@hotmail.com>
2025-08-21 14:08:55 -0300

    instalação e configuração do tailwind

--- src/components/BaseButton/index.tsx
+++ src/components/BaseButton/index.tsx
@@ -1,14 +1,14 @@
 import type { JSX } from "react";
 
 export interface BaseButtonProps {
-	text: string;
-	onClick?: () => void;
+  text: string;
+  onClick?: () => void;
 }
 
 export const BaseButton = ({ onClick, text }: BaseButtonProps): JSX.Element => {
-	return (
-		<button onClick={onClick} data-testid="Base-Button-Component">
-			{text}
-		</button>
-	);
+  return (
+    <button onClick={onClick} data-testid="Base-Button-Component">
+      {text}
+    </button>
+  );
 };
