6c5acf21c8e521b1cb89aa9dfbecfbb476b15c8d
lauranarciso <lauranarcisop@hotmail.com>
2025-09-18 21:11:44 -0300

    inicio da tela de perfil

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,13 +2,15 @@ import { Routes, Route } from "react-router-dom";
 import { Login } from "./pages/Login";
 import { Cadastro } from "./pages/Cadastro";
 import { Home } from "./pages/Home";
+import { MeuPerfil } from "./pages/MeuPerfil";
 
 export const RoutesManager = () => {
-	return (
+  return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/cadastro" element={<Cadastro />} />
       <Route path="/home" element={<Home />} />
+      <Route path="/meu-perfil" element={<MeuPerfil />} />
     </Routes>
   );
 };
---
+++ src/pages/MeuPerfil/index.tsx
@@ -0,0 +1,11 @@
+import { Card } from "@/components/Card";
+
+export const MeuPerfil = () => {
+  return (
+    <Card>
+      <h1 className="text-xl text-foreground font-semibold">
+        Informações Pessoais
+      </h1>
+    </Card>
+  );
+};
